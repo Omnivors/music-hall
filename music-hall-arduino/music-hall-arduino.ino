ffd5561710421dd77405ea9f73e9828120663dbf
Francesco Cretti <francesco.cretti@gmail.com>
2018-03-17 13:36:54 +0100

    code cleaned

--- music-hall-arduino/music-hall-arduino.ino
+++ music-hall-arduino/music-hall-arduino.ino
@@ -1,60 +1,40 @@
 //HC RS04 Sensore ultrasuoni
-//Giuseppe Caccavale
 const int triggerPort = 9;
 const int echoPort = 10;
-//const int led = 13;
 int freqVal;
 
 void setup() {
-
-pinMode(triggerPort, OUTPUT);
-pinMode(echoPort, INPUT);
-pinMode(LED_BUILTIN, OUTPUT);
-Serial.begin(9600);
-// Serial.print( "Sensore Ultrasuoni: ");
+  pinMode(triggerPort, OUTPUT);
+  pinMode(echoPort, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  // porta bassa l'uscita del trigger
+  digitalWrite( triggerPort, LOW );
+  // invia un impulso di 10microsec su trigger
+  digitalWrite( triggerPort, HIGH );
+  delayMicroseconds( 10 );
+  digitalWrite( triggerPort, LOW );
 
-//porta bassa l'uscita del trigger
-digitalWrite( triggerPort, LOW );
-//invia un impulso di 10microsec su trigger
-digitalWrite( triggerPort, HIGH );
-delayMicroseconds( 10 );
-digitalWrite( triggerPort, LOW );
+  long durata = pulseIn( echoPort, HIGH );
+  long distanza = 0.034 * durata / 2;
 
-long durata = pulseIn( echoPort, HIGH );
+  //dopo 38ms è fuori dalla portata del sensore
+  if (durata > 38000) {
+    Serial.write(0);
+  } else {
+    freqVal = map(distanza, 3, 30, 255, 0);
+    freqVal = constrain(freqVal, 0, 255);
+    Serial.write(freqVal);
+  }
 
-long distanza = 0.034 * durata / 2;
+  if (distanza < 10) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
-// Serial.print("freq: ");
-
-//dopo 38ms è fuori dalla portata del sensore
-if( durata > 38000 ){
-//Serial.println("Fuori portata   ");
-Serial.write(0);
-}
-else{ 
-//Serial.print(" cm: ");
-//Serial.println(distanza); 
-
-// Serial.write(distanza);
-freqVal = map(distanza, 3, 30, 255, 0);
-//Serial.print(distanza);
-//Serial.println(" cm");
-freqVal = constrain(freqVal, 0, 255);
-//Serial.println(freqVal);
-Serial.write(freqVal);
-//Serial.println(" Hz     ");
-}
-
-if(distanza < 10){
- digitalWrite(LED_BUILTIN, HIGH);
-}
-else{
- digitalWrite(LED_BUILTIN, LOW);
-}
-
-//Aspetta 1000 microsecondi
-delay(60);
+  delay(60);
 }
