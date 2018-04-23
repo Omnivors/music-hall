ed249ea5e8cadb18fc99dacd8c2c153d7449ccf0
Francesco Cretti <francesco.cretti@gmail.com>
2018-04-23 20:35:37 +0200

    chained sensors managment

--- music-hall-arduino/music-hall-arduino.ino
+++ music-hall-arduino/music-hall-arduino.ino
@@ -14,18 +14,30 @@ const int sensorPin[3] = {0, 1, 2};
 unsigned int rangeCm[3];
 // SMOOTHING METHOD - exponentially decaying moving average - equivalent window = 10
 const float tiny=1-(1/10.0); 
+// Trigger
+const int triggerPin = 2;
 
 /******* SETUP *********/
 void setup () {
   Serial.begin(9600);
+  pinMode(triggerPin,OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  delay(300);
 }
 
 /******* LOOP *********/
 void loop () {
+  start_sensor();
+  
   readSensor(0);
-  sendData();
-  delay(100);
+  readSensor(1);
+  readSensor(2); 
+  
+  sendData(0);
+  sendData(1);
+  sendData(2);
+  
+  delay(300);
 }
 
 void readSensor (int sensor) {
@@ -41,10 +53,37 @@ void readSensor (int sensor) {
 
 void sendData () {
   // Send sensor 1 data
-  uint8_t LSB = rangeCm[0];
-  uint8_t MSB = rangeCm[0] >> 8;
-  Serial.write(MSB);
-  Serial.write(LSB);
-  //Serial.println(rangeCm[0]);
+  uint8_t LSB0 = rangeCm[0];
+  uint8_t MSB0 = rangeCm[0] >> 8;
+  uint8_t LSB1 = rangeCm[1];
+  uint8_t MSB1 = rangeCm[1] >> 8;
+  uint8_t LSB2 = rangeCm[2];
+  uint8_t MSB2 = rangeCm[2] >> 8;
+  //Serial.write(MSB);
+  //Serial.write(LSB);
+  Serial.print("sensor 0: ");
+  Serial.println(rangeCm[0]);
+  Serial.print("sensor 1: ");
+  Serial.println(rangeCm[1]);
+  Serial.print("sensor 2: ");
+  Serial.println(rangeCm[2]);
   // Serial.write(rangeCm[0]);
+}
+
+void sendData (int sensor) {
+  // Send sensor 1 data
+  uint8_t LSB0 = rangeCm[sensor];
+  uint8_t MSB0 = rangeCm[sensor] >> 8;
+  //Serial.write(MSB);
+  //Serial.write(LSB);
+  Serial.print("sensor ");
+  Serial.print(sensor);
+  Serial.print(": ");
+  Serial.println(rangeCm[sensor]);
+}
+
+void start_sensor() {
+  digitalWrite(triggerPin,HIGH);
+  delay(1);
+  digitalWrite(triggerPin,LOW);
 }
